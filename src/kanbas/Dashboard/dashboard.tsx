d08542b331bdd510388f3679f1ae19e93a4e25aa
pew35_4366997 <pew35@pitt.edu>
2024-12-06 01:23:08 -0800

    Update dashboard.tsx

--- src/kanbas/Dashboard/dashboard.tsx
+++ src/kanbas/Dashboard/dashboard.tsx
@@ -96,7 +96,7 @@ export default function Dashboard() {
         fetchEnrollments();
         fetchallCourses();
         fetchUserCourses();
-    }, [courses, allcourses,enrollments]);
+    }, [enrollments]);
 
 
     return (
